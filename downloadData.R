15bbdf58ea33e1b057ac626a1e5682a5de537087
Nathan Stephens <nathan@rstudio.com>
2021-10-14 16:48:52 +0000

    adding speaker notes

--- downloadData.R
+++ downloadData.R
@@ -1,6 +1,12 @@
 library(vroom)
+
+# Download
 download.file("https://www.ssa.gov/oact/babynames/state/namesbystate.zip", "namesbystate.zip")
 unzip("namesbystate.zip", exdir = "namesbystate")
+
+# Read
 files <- dir("namesbystate", pattern = "*.TXT", full.names = TRUE)
 dat <- vroom(files, col_names=c("state", "sex", "year", "name", "n"))
+
+# Write
 vroom_write(dat, "app/namesbystate.csv", delim = ",")
